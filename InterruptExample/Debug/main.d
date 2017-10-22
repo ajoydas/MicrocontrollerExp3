e2a3c8ec2549992acffd17367874ce92bd7f6bd8
Ajoy Das <1405079.ad@ugrad.cse.buet.ac.bd>
2017-10-22 13:00:16 +0600

    Added one button toggle pooling

---
+++ InterruptExample/Debug/main.d
@@ -0,0 +1,49 @@
+main.d main.o: .././main.c \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ F:\InstalledProgram\AtmelStudio7\7.0\Packs\atmel\ATmega_DFP\1.2.132\include/avr/iom32.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+F:\InstalledProgram\AtmelStudio7\7.0\Packs\atmel\ATmega_DFP\1.2.132\include/avr/iom32.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\delay.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+f:\installedprogram\atmelstudio7\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ InterruptExample/main.c
@@ -0,0 +1,54 @@
+/*
+ * interrupt.c
+ *
+ * Created: 16-10-2017 14:56:48
+ * Author : hp
+ */ 
+
+#include <avr/io.h>
+#include "avr/interrupt.h"
+#include <avr/delay.h>
+
+//ISR(INT0_vect)
+//{
+	//PORTB = ~PORTB;
+//
+//}
+//
+//int main()
+//{
+	//DDRB = 0xff;  //Port b output configure
+	//PORTB = 0b01010101;
+	//MCUCR = 0b00000000; //Toggle when it is in pressed or low level
+	//GICR = (1<<INT0);
+	//sei();
+	//while(1);  // Alive loop 
+//
+//}
+
+int main(void)
+{
+	DDRC = 0x01;
+	PORTC = 0x00;
+	DDRB = 0xfe;
+	while(1)
+	{
+		//if(PORTC != 0x01){
+			//PORTC = 0x01;
+		//}
+		//else PORTC = 0x00;
+		//_delay_ms(2000);
+
+		if(PINB & (1<<PB0))
+		{
+			if(PORTC != 0x01){
+				PORTC = 0x01;
+			}
+			else PORTC = 0x00;
+		}
+		_delay_ms(2000);
+
+
+	}
+}
+
